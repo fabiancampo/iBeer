00c6007636e84ec4d5eaeec6bbac67dc4576b4b4
Fabián Gómez Campo <fabian.gomez@usal.es>
2023-11-24 12:27:10 +0100

    Initial Commit

---
+++ iBeer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iBeer
+//
+//  Created by Fabián Gómez Campo on 24/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iBeer/iBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBeerApp.swift
+//  iBeer
+//
+//  Created by Fabián Gómez Campo on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
